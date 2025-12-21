34faca9bb1620e10b1d2a7d69ee76d3c9fb92a88
TheRamAlakazaam <6448236+TheRamAlakazaam@users.noreply.github.com>
2025-12-21 05:21:29 -0600

    tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: cloudflare()
 });
